7f943a34a7458fcbdce87b006f5fe0f9e662e14f
Anas <anasfarhad304@gmail.com>
2023-07-21 20:39:26 +0500

    initial commit

---
+++ app/src/main/java/com/example/viewmodel2023/BaseViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.viewmodel2023
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.ViewModel
+
+class BaseViewModel : ViewModel()  {
+
+    var backgroundColor by mutableStateOf(Color.White)
+        private set
+
+    fun changeBackgroundColor(){
+        backgroundColor = Color.Green
+    }
+}
---
+++ app/src/main/java/com/example/viewmodel2023/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.viewmodel2023
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.viewmodel2023.ui.theme.ViewModel2023Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+
+            ViewModel2023Theme {
+                val viewModel : BaseViewModel = viewModel()
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = viewModel.backgroundColor
+                ) {
+                    Button(onClick = { viewModel.changeBackgroundColor() }) {
+                        Text(text = "Change color")
+                    }
+                }
+            }
+
+        }
+    }
+}
+
